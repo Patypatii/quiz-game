bbc833250f0b1abd283685de661c10d79ddabf94
Patypatii <patrickwambugu707@gmail.com>
2025-07-28 07:17:08 +0300

    added the questions and answeds

--- quiz_game.py
+++ quiz_game.py
@@ -6,3 +6,47 @@ if playing != "yes":
     quit()
 
 print("Okay! Let's play:)")
+
+answer = input("What does CPU stand for? ")
+
+if answer == "central processing unit":
+    print('correct!')
+
+else: 
+    print('Incorrect!')
+
+
+answer = input("What does GPU stand for? ")
+
+if answer == "graphics processing unit":
+    print('correct!')
+
+else: 
+    print('Incorrect!')
+
+
+answer = input("What does RAM stand for? ")
+
+if answer == "random access memory":
+    print('correct!')
+
+else: 
+    print('Incorrect!')
+
+
+answer = input("What does PSU stand for? ")
+
+if answer == "power supply unit":
+    print('correct!')
+
+else: 
+    print('Incorrect!')
+
+
+answer = input("What does ROM stand for? ")
+
+if answer == "read only memory":
+    print('correct!')
+
+else: 
+    print('Incorrect!')
